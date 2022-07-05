bef677647942acff3e1d36a71aab6564a308f265
Golam Wasy Arnob <wagolam2@graduate.utm.my>
2022-07-05 17:57:12 +0800

    Created and initialized spring boot web application

---
+++ src/main/java/com/example/scmasterminds/ScMasterMindsApplication.java
@@ -0,0 +1,13 @@
+package com.example.scmasterminds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScMasterMindsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScMasterMindsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/scmasterminds/ScMasterMindsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.scmasterminds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScMasterMindsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
