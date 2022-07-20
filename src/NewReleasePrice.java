1f8d6bc81b103c4098d5a895ac29a879c725bfc6
Tausif Hossain <tsftousif2@gmail.com>
2022-07-20 22:44:00 +0800

    MMP-13 Applied polymorphism for getCharge method

--- src/NewReleasePrice.java
+++ src/NewReleasePrice.java
@@ -2,4 +2,12 @@ public class NewReleasePrice extends Price {
     int getPriceCode() {
         return Movie.NEW_RELEASE;
     }
+
+    public double getCharge(int daysRented){
+        return daysRented * 3;
+    }
+
+    public int getFrequentRenterPoints(int daysRented) {
+        return (daysRented > 1) ? 2: 1;
+    }
 }
